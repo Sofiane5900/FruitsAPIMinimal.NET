7acf3b2ed8983d17ee9bfc6bef2ae87fc49f1290
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-06 16:50:23 +0100

    sauvegarde 16:50

--- FruitsAPI.NET/Program.cs
+++ FruitsAPI.NET/Program.cs
@@ -3,15 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Swagger Doc
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("postgres"))
 );
-
-// Swager
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +20,37 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.MapGet("/fruits", async (AppDbContext db) => await db.Fruits.ToArrayAsync());
+
+app.MapPost(
+    "/fruits",
+    async (AppDbContext db, Fruits fruits) =>
+    {
+        db.Fruits.Add(fruits);
+        await db.SaveChangesAsync();
+
+        return Results.Created($"/fruits/{fruits.ID}", fruits);
+    }
+);
+
+app.MapPut(
+    "/fruits/{id}",
+    async (int id, Fruits updatedFruits, AppDbContext db) =>
+    {
+        var fruits = await db.Fruits.FindAsync(id);
+        if (fruits is null)
+        {
+            return Results.NotFound();
+        }
+
+        fruits.Nom = updatedFruits.Nom;
+        fruits.Saison = updatedFruits.Saison;
+
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+);
 
 app.UseHttpsRedirection();
 
