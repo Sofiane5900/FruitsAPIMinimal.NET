5aefd0a5f5239704bbe6f2ccd99983049beb47c0
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-06 15:45:00 +0100

    ajout connection postgre

--- FruitsAPI.NET/Fruits.cs
+++ FruitsAPI.NET/Fruits.cs
@@ -1,9 +1,16 @@
-﻿namespace FruitsAPI.NET
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FruitsAPI.NET
 {
     public class Fruits
     {
+        [Key]
         public int ID { get; set; }
+
+        [Required]
         public string? Nom { get; set; }
+
+        [Required]
         public string? Saison { get; set; }
     }
 }
