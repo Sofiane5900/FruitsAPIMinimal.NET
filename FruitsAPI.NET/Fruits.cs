23a06d56ed18685e181624f784b6897b14a808bd
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-06 15:18:30 +0100

    add files

---
+++ FruitsAPI.NET/Fruits.cs
@@ -0,0 +1,9 @@
+﻿namespace FruitsAPI.NET
+{
+    public class Fruits
+    {
+        public int ID { get; set; }
+        public string? Nom { get; set; }
+        public string? Saison { get; set; }
+    }
+}
